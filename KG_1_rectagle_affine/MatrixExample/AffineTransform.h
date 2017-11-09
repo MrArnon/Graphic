6355df3e52a022bc6874739b6defe113a91bb9c9
MrArnon <32636989+MrArnon@users.noreply.github.com>
2017-11-09 12:18:42 +0300

    stage2 affine + rectagle matrix

---
+++ KG_1_rectagle_affine/MatrixExample/AffineTransform.h
@@ -0,0 +1,72 @@
+#ifndef AFFINE_TRANSFORM_H
+#define AFFINE_TRANSFORM_H
+
+Matrix<> Translation(double x, double y)
+{
+	double T[9] = {
+		1, 0, x,
+		0, 1, y,
+		0, 0, 1};
+	return Matrix<>(3,3, T);
+}
+Matrix<> Rotation(double t)
+{
+	double R[9] = {
+		cos(t), -sin(t), 0,
+		sin(t), cos(t), 0,
+		0,		0,		1 };
+	return Matrix<>(3,3, R);
+}
+Matrix<> Rotation(double c,double s)
+{
+	double R[9] = {
+		c, -s, 0,
+		s, c, 0,
+		0,0,1 };
+	return Matrix<>(3,3, R);
+}
+Matrix<> Scaling(double kx, double ky)
+{
+	double S[9] = {
+		kx, 0, 0,
+		0, ky, 0,
+		0,0,1 };
+return Matrix<>(3,3, S);
+}
+Matrix<> Mapping(double x)
+{
+	double M[9] = {
+		1, 0, 0,
+		0, -1, 0,
+		0,0,1 };
+	return Matrix<>(3,3, M);
+}
+/*Matrix<> Mapping(double y)
+{
+	double M[9] = {
+		-1, 0, 0,
+		0, 1, 0,
+		0,0,1 };
+	return Matrix<>(3,3, M);
+}
+Matrix<> Mapping(double x,double y)
+{
+	double M[9] = {
+		-1, 0, 0,
+		0, -1, 0,
+		0,0,1 };
+	return Matrix<>(3,3, M);
+}*/
+// Здесь требуется реализовать функции, возвращающие матрицы базовых АП:
+// Identity() - тождественное АП;
+// Rotation(t) - поворот на угол t;
+// Rotation(c, s) - поворот на угол, косинус и синус которого пропорциональны величинам c и s;
+// Scaling(kx, ky) - масштабирование;
+// Mapping (различные виды отражений) - по желанию, для создания матриц отражения можно использовать функцию Scaling.
+
+// В приведённом примере используется конструктор матрицы из списка.
+// Приветствуются идеи о других способах создания матриц.
+// Например, вызов функции, создающей единичную матрицу,
+// с последующим переопределением некоторых её коэффициентов.
+
+#endif AFFINE_TRANSFORM_H
---
+++ KG_1_rectagle_affine/MatrixExample/Matrix.h
@@ -0,0 +1,195 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+
+using namespace std;
+
+
+template <typename Cell = double>
+class Matrix
+{
+private:
+	//int size;
+	int rows;
+	int cols;
+	Cell **cells;
+	void AllocateCells(int,int);
+	void FreeCells();
+public:
+	Matrix() : size(0), cells(nullptr) {}	// Конструктор по умолчанию
+	Matrix(const Matrix&);					// Конструктор копирования
+	Matrix(int,int);							// Конструктор нулевой матрицы
+	Matrix(int,int, Cell*);						// Конструктор матрицы из списка
+	~Matrix();								// Деструктор
+
+	Cell &operator()(int i, int j) { return cells[i-1][j-1]; }
+	
+	Matrix& operator = (const Matrix&);		// Перегрузка оператора присваивания
+	Matrix  operator + (const Matrix&);		// Сложение матриц
+	Matrix  operator - (const Matrix&);		// Вычитание матриц
+	Matrix  operator * (const Matrix&);		// Умножение матриц
+
+	friend istream& operator >> <> (istream&, Matrix&);			// Перегрузка оператора >> для ввода матрицы
+	//friend ostream& operator << <> (ostream&,  const Matrix&);	// Перегрузка оператора << для вывода матрицы
+	friend ostream& operator << <> (ostream& fo, const Matrix<Cell>& M);
+};
+
+
+template <typename Cell>
+Matrix<Cell>::Matrix(const Matrix<Cell>& M)
+{
+	AllocateCells(M.rows,M.cols);
+	for (int i=0; i<rows; i++)
+	for (int j=0; j<cols; j++)
+		cells[i][j] = M.cells[i][j];
+}
+
+template <typename Cell>
+Matrix<Cell>::Matrix(int Rows,int Cols)
+{
+	AllocateCells(Rows, Cols);
+	for (int i=0; i<rows; i++)
+	for (int j=0; j<cols; j++)
+		cells[i][j] = 0;
+}
+
+template <typename Cell>
+Matrix<Cell>::Matrix(int Rows,int Cols, Cell* list)
+{
+	//Matrix<Cell> res(Size);
+	//AllocateCells(M.rows, M.cols);
+	AllocateCells(Rows, Cols);
+	int r = 0;
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+		{
+			cells[i][j] = list[r];
+			r++;
+			//list = list->next;
+		}
+	//return res;
+	/* ... */
+}
+
+template <typename Cell>
+Matrix<Cell>::~Matrix()
+{
+	FreeCells();
+}
+
+template <typename Cell>
+Matrix<Cell>& Matrix<Cell>::operator=(const Matrix& M)
+{
+	if ( rows != M.rows || cols != M.cols )
+	{
+		FreeCells();
+		AllocateCells(M.rows,M.cols);
+	}
+	for (int i=0; i<rows; i++)
+	for (int j=0; j<cols; j++)
+		cells[i][j] = M.cells[i][j];
+	return *this;
+}
+
+template <typename Cell>
+Matrix<Cell> Matrix<Cell>::operator+(const Matrix& M)
+{
+	Matrix<Cell> res(*this);
+	if (rows == M.rows && cols == M.cols)
+	{
+		for (int i=0; i<rows; i++)
+		for (int j=0; j<cols; j++)
+			res.cells[i][j] += M.cells[i][j];
+	}
+	return res;
+}
+
+template <typename Cell>
+Matrix<Cell> Matrix<Cell>::operator-(const Matrix& M)
+{
+	Matrix<Cell> res(*this);
+	if (rows == M.rows && cols == M.cols)
+	{
+		for (int i = 0; i<rows; i++)
+			for (int j = 0; j<cols; j++)
+				res.cells[i][j] -= M.cells[i][j];
+	}
+	return res;
+	/* ... */
+	//return Matrix();
+}
+
+template <typename Cell>
+Matrix<Cell> Matrix<Cell>::operator*(const Matrix& M)
+{
+
+	
+	Matrix<Cell> res(*this);
+	Matrix<Cell> S(rows,cols);
+	if (rows == M.rows && cols == M.cols)
+	{
+		for (int i = 0; i<rows; i++)
+			for (int j = 0; j < cols; j++)
+			{
+				double temp = 0;
+				for (int k = 0; k < cols; k++)
+				{
+				//	S.cells[i][j] += res.cells[i][k] * M.cells[k][j];
+					temp+= res.cells[i][k] * M.cells[k][j];
+					//cout << "!!!!!!" << res.cells[i][k];
+				}
+				S.cells[i][j] = temp;
+			}
+			}
+	return S;
+	/* ... */
+	//return Matrix();
+}
+
+template <typename Cell>
+void Matrix<Cell>::AllocateCells(int Rows,int Cols)
+{
+	cells = new Cell*[Rows];
+	for (int i=0; i<Cols; i++)
+		cells[i] = new Cell[Rows];
+	rows = Rows;
+	cols = Cols;
+}
+
+template <typename Cell>
+void Matrix<Cell>::FreeCells()
+{
+	for (int i=0; i<cols; i++)
+		delete cells[i];
+	delete cells;
+	cols = 0;
+	rows = 0;
+}
+
+
+template <typename Cell>
+istream& operator >> (istream& fi, Matrix<Cell>& M)
+{
+	for (int i=0; i<M.rows; i++)
+	for (int j=0; j<M.cols; j++)
+		fi >> M.cells[i][j];
+	return fi;
+}
+
+template <typename Cell>
+ostream& operator << <>(ostream& fo, const Matrix<Cell>& M)
+{
+	for (int i=0; i<M.rows; i++)
+	{
+		fo << "  ";
+		for (int j=0; j<M.cols; j++)
+			fo << M.cells[i][j] << " \t";
+		fo << endl;
+	}
+	return fo;
+}
+
+
+
+#endif MATRIX_H
